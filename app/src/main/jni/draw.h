d0c9ea2a1cc020070fbe5c069af9046291fabbdc
xabii666 <lionparcelpangeranderajat@gmail.com>
2026-06-17 17:41:21 +0700

    Update draw.h

--- app/src/main/jni/draw.h
+++ app/src/main/jni/draw.h
@@ -1164,7 +1164,7 @@ static void DrawContentArea(float sidebarW, float winW, float winH, ImVec2 winPo
 
     // ── Slider X ──
     float sliderX = persistent_float["fPSliderX"];
-    if (GoldSliderFloat("X Position", "Horizontal", &sliderX, 0.50f, 1.0f, "%.3f")) {
+    if (GoldSliderFloat("Drag Sensitivity", "Pixels per radian", &sensitivity, 50.0f, 1500.0f, "%.0f")) {
     persistent_float["fPSliderX"] = sliderX;
     need_save = true;
     }
