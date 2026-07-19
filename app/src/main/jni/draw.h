88d528751996928d322449ec1276ca242093363e
Dewaa. <92504311+dewaaaxxx@users.noreply.github.com>
2026-07-19 21:00:27 +0700

    Update draw.h

--- app/src/main/jni/draw.h
+++ app/src/main/jni/draw.h
@@ -30,7 +30,7 @@ using namespace std;
 
 #include "8bp/FrictionProperties.h"
 #include "ButtonClicker.h"
-#include "8bp/inc/AutoAim2.h"
+//#include "8bp/inc/AutoAim2.h"
 #include "8bp/inc/AutoPlay_impl-2.h"
 #include "logo.h"
 #include "on.h"
