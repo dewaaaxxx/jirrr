c1d970431cd7fc10a062cf47ed3ffed5e9d5384d
Dewaa. <92504311+dewaaaxxx@users.noreply.github.com>
2026-07-25 13:46:40 +0700

    Update 8bp.h

--- app/src/main/jni/8bp.h
+++ app/src/main/jni/8bp.h
@@ -14,6 +14,6 @@
 #include "8bp/inc/8bp.h"
 #include "8bp/inc/GameConstants.h"
 #include "8bp/inc/NumberUtils.h"
-// #include "8bp/inc/AutoAim.h"
+#include "8bp/inc/AutoAim.h"
 #include "8bp/inc/AutoPlay.h"
 #include "8bp/inc/AutoQueue.h"
